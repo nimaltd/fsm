4ecf78f634a68af724bd4d6c9b08d1ea06c5db90
Nima Askari (نیما عسکری) <nimaltd@yahoo.com>
2025-09-29 17:50:44 +0200

    V1.0.0

---
+++ fsm_config.h
@@ -0,0 +1,46 @@
+
+/*
+ * @file        fsm_config.h
+ * @brief       Finite State Machine + task manager Library
+ * @author      Nima Askari
+ * @version     1.0.0
+ * @license     See the LICENSE file in the root folder.
+ *
+ * @note        All my libraries are dual-licensed.
+ *              Please review the licensing terms before using them.
+ *              For any inquiries, feel free to contact me.
+ *
+ * @github      https://www.github.com/nimaltd
+ * @linkedin    https://www.linkedin.com/in/nimaltd
+ * @youtube     https://www.youtube.com/@nimaltd
+ * @instagram   https://instagram.com/github.nimaltd
+ *
+ * Copyright (C) 2025 Nima Askari - NimaLTD. All rights reserved.
+ */
+
+#ifndef _FSM_CONFIG_H_
+#define _FSM_CONFIG_H_
+
+/*************************************************************************************************/
+/** Includes **/
+/*************************************************************************************************/
+
+/* USER CODE BEGIN FSM_INCLUDES */
+
+/* USER CODE END FSM_INCLUDES */
+
+/*************************************************************************************************/
+/** Configurations **/
+/*************************************************************************************************/
+
+/* USER CODE BEGIN FSM_CONFIGURATION */
+
+#define FSM_MAX_TASKS       16
+
+/* USER CODE END FSM_CONFIGURATION */
+
+/*************************************************************************************************/
+/** End of File **/
+/*************************************************************************************************/
+
+#endif /* _FSM_CONFIG_H_ */
